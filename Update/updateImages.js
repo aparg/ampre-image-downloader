40afe0e19d04d379fa3407a72a0815cdd91a6eff
aparg <apargtm@gmail.com>
2025-09-11 09:24:13 +0545

    fix: module import

--- Update/updateImages.js
+++ Update/updateImages.js
@@ -62,7 +62,7 @@ const updateImages = async () => {
   while (keepGoing) {
     // Fetch listings modified or with media changed in the last hour
     // let filter = `(ModificationTimestamp gt ${lastTimestamp} or (ModificationTimestamp eq ${lastTimestamp} and ListingKey gt 'X12314516') or MediaChangeTimestamp gt ${oneHourAgo}) and ContractStatus eq 'Available' and StandardStatus eq 'Active'`;
-    let filter = `(${cityFilter}) and ((ModificationTimestamp ge ${lastTimestamp} and ModificationTimestamp le ${nowTime}  and ListingKey gt '${lastListingKey}') or MediaChangeTimestamp gt ${oneHourAgo}) and ContractStatus eq 'Available' and StandardStatus eq 'Active'`;
+    let filter = `(${cityFilter}) and ((ModificationTimestamp ge ${lastTimestamp} and ModificationTimestamp le ${nowTime}  and ListingKey gt '${lastListingKey}') or (MediaChangeTimestamp gt ${oneHourAgo}) and ContractStatus eq 'Available' and StandardStatus eq 'Active'))`;
     const url = `https://query.ampre.ca/odata/Property?$filter=${encodeURIComponent(
       filter
     )}&$select=ListingKey,ModificationTimestamp,MediaChangeTimestamp&$top=500&$orderby=ModificationTimestamp,ListingKey`;
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-// const sharp = require("sharp");
+const sharp = require("sharp");
 const fs = require("fs");
 const path = require("path");
 
